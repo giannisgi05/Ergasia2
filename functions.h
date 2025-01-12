ee409352140ff56f1248df7add28e080f62ea9ea
giannisgi05 <giannisgiatsas2005@gmail.com>
2025-01-12 19:50:05 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,18 +1,23 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
+
 #include <stdbool.h>
+
 #define NUM_PRODUCTS 20
 #define NUM_CUSTOMERS 5
 #define NUM_ORDERS_PER_CUSTOMER 10
 #define PROCESSING_TIME 1 // in seconds
+
 typedef struct {
-char description[50];
-float price;
-int item_count;
-int requests;
-int total_requests;
+    char description[50];
+    float price;
+    int item_count;
+    int requests;
+    int total_requests;
 } Product;
+
 // Δηλώσεις συναρτήσεων
 void initializeCatalog();
 void processOrder(int customer_id, int product_index, int* success, float* total_cost);
+
 #endif // FUNCTION_H
